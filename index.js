bc4e60ae2c56163e3ee069d80640183cd522e7cc
piyushtakale <piyusht265@gmail.com>
2020-03-13 10:50:28 +0530

    castling

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ for(var i=0; i<8;i++){
     }
     cboard.innerHTML += "</tr>";
 }
-
+var castling={"white":{"left":[71,72,73],"right":[75,76]},"black":{"left":[01,02,03],"right":[05,06]}};
+var capable_castling={"white":{"i":7,"right":true,"left":true},"black":{"i":0,"right":true,"left":true}};
 //setting position of troop
 var ind=0,jnd=0;
 var pos={
@@ -61,6 +62,7 @@ for(color in pos){
 }
 var human="white",ai="black";
 var user=human,checkbit=false;
+console.log(user);
 var possmov={};
 var board=[];
 function update(){
@@ -72,7 +74,7 @@ function update(){
     }
 
 }
-
+console.log(user);
 function autocalculate(){
     var possmov={}; //this variable is diffrant than global one
     for(var i=0;i<8;i++){
@@ -96,7 +98,7 @@ function autocalculate(){
 
                                 }else{
                                     var x=i-1;
-                                    if(board[x][j].length==0){
+                                    if(x>=0 && board[x][j].length==0){
                                         possmov[i+""+j][0]=x+""+j;
                                     }
                                 }
@@ -113,7 +115,7 @@ function autocalculate(){
                                     }
                                 } else{
                                     var x=i+1;
-                                    if(board[x][j].length==0){
+                                    if(x<8 && board[x][j].length==0){
                                         possmov[i+""+j][0]=x+""+j;
                                     }
                                 }
@@ -125,7 +127,7 @@ function autocalculate(){
                                 var x=i+1;
                             }
                             var y=j-1;
-                            if(y>=0){
+                            if(y>=0 && x>=0 && x<8){
                                 if(board[x][y].length>0){
                                     if(board[x][y][0].getAttribute("class") != user){
                                         possmov[i+""+j][len++]=x+""+y;
@@ -133,7 +135,7 @@ function autocalculate(){
                                 }
                             }
                             y=j+1;
-                            if(y<8){
+                            if(y<8 && x>=0 && x<8){
                                 if(board[x][y].length>0){
                                     if(board[x][y][0].getAttribute("class") != user){
                                         possmov[i+""+j][len++]=x+""+y;
@@ -472,7 +474,26 @@ function autocalculate(){
                                 if(x>=0 && (board[i][x].length==0 || board[i][x][0].getAttribute("class")!=user)){
                                     possmov[i+""+j][inc++]=i+""+x;
                                 }
+                                x=i-1;
+                                var y=j-1;
+                                if(x>=0 && y>=0 && (board[x][y].length==0 || board[x][y][0].getAttribute("class")!=user)){
+                                    possmov[i+""+j][inc++]=x+""+y;
+                                }
+                                
+                                x=i-1,y=j+1;
+                                if(x>=0 && y<8 && (board[x][y].length==0 || board[x][y][0].getAttribute("class")!=user)){
+                                    possmov[i+""+j][inc++]=x+""+y;
+                                }
+                                x=j+1,y=j+1;
+                                if(x<8 && y<8 &&(board[x][y].length==0 || board[x][y][0].getAttribute("class")!=user)){
+                                    possmov[i+""+j][inc++]=x+""+y;
+                                }
+                                y=j-1,x=i+1;
+                                if(x<8 && y>=0 &&(board[x][y].length==0 || board[x][y][0].getAttribute("class")!=user)){
+                                    possmov[i+""+j][inc++]=x+""+y;
+                                }
                                 break;
+
                     }
                 }
             }
@@ -509,51 +530,146 @@ function getfilter(possmov){
                 board[dest_i][dest_j]=temp;
             }
         }
+        if(!checkbit){
+            if(capable_castling[user]["left"]){
+                var color=capable_castling[user]["i"];
+                if( board[color][2].length==0 && board[color][1].length==0 && lim_pos[color+"4"].includes(color+"3")){
+                    swapuser();
+                    var cas_pos=autocalculate();
+                    var bit=true;
+                    for(arre in cas_pos){
+                        if(cas_pos[arre].includes(color+"2")){
+                            bit= false;
+                            break;
+                        }
+                    }
+                    swapuser();
+                    if(bit){
+                        lim_pos[color+"4"].push(color+"2");
+                    }
+                }  
+            }
+            if(capable_castling[user]["right"]){
+                var color=capable_castling[user]["i"];
+                if( board[color][6].length==0 && lim_pos[color+"4"].includes(color+"5")){
+                    swapuser();
+                    var cas_pos=autocalculate();
+                    var bit=true;
+                    for(arre in cas_pos){
+                        if(cas_pos[arre].includes(color+"6")){
+                            bit= false;
+                            break;
+                        }
+                    }
+                    swapuser();
+                    if(bit){
+                        lim_pos[color+"4"].push(color+"6");
+                    }
+                }  
+            }
+        }
+
         return lim_pos;
 }
 update();
 possmov=getfilter(autocalculate());
-
+var prev=possmov;
 var cur_pos='';
 var currenttroop='';
 var cur_tr_po_mov=[];
 var cur_tr_po_mov_bg=[];
+
+
 var cboard = document.getElementById('chessboard');
 function markPossible(source){
     
     if(source.style.backgroundColor=="rgb(140, 246, 255)"){
-        source.innerHTML=currenttroop;
+        source.innerHTML=currenttroop.innerHTML;
         document.getElementById(cur_pos).innerHTML='';
         for(var i=0;i<cur_tr_po_mov.length;i++){
             document.getElementById(cur_tr_po_mov[i]).style.background=cur_tr_po_mov_bg[i];
         }
-        
-
+            if(source.children[0].getAttribute("name")=="king"){
+                if(currenttroop.getAttribute("id")==capable_castling[user]["i"]+"4"){
+                    if(source.getAttribute("id")==capable_castling[user]["i"]+"2"){
+                        document.getElementById(capable_castling[user]["i"]+"3").innerHTML=document.getElementById(capable_castling[user]["i"]+"0").innerHTML;
+                        document.getElementById(capable_castling[user]["i"]+"0").innerHTML="";
+                    }
+                    else if(source.getAttribute("id")==capable_castling[user]["i"]+"6"){
+                        document.getElementById(capable_castling[user]["i"]+"5").innerHTML=document.getElementById(capable_castling[user]["i"]+"7").innerHTML;
+                        document.getElementById(capable_castling[user]["i"]+"7").innerHTML="";
+                    }
+                }
+            }
+            if(source.children[0].getAttribute("name")=="king"){
+                   capable_castling[user]["left"] =false;
+                capable_castling[user]["right"] =false;
+            }
+            if(source.children[0].getAttribute("name")=="elephant"){
+                
+                if(currenttroop.getAttribute("id")==capable_castling[user]["i"]+"0"){
+                    capable_castling[user]["left"] =false; 
+                }
+            }
+            if(source.children[0].getAttribute("name")=="slder"){
+                var value
+                if(Math.floor(source.getAttribute("id")/10)==0){
+                    document.getElementById("choose").style.display="block";
+                    document.getElementById("choose").addEventListener("change",function(){
+                        value= document.getElementById("choose").value;
+                        source.innerHTML=value;
+                    });
+                    //document.getElementById("choose").style.display="none";
+                    
+                }
+            }
         cur_pos='';
         currenttroop='';
         cur_tr_po_mov=[];
         cur_tr_po_mov_bg=[];
 
+        
+
         update();
+        var res;
         if(ifCheck()){
-            alert("check");
+            res=true;
+            checkbit=true;
         }
+        
         if(user==human){
             user=ai;
         } else{
             user=human;
         }
+        
+        
         possmov=getfilter(autocalculate());
+        
+        if(res){
+            
+            var mate=true;
+            for(arr in possmov){
+                if(possmov[arr].length>0){
+                    alert("check");
+                    mate=false;
+                    break;
+                }
+            }
+            if(mate){
+                alert("checkmate");
+            }
+        }
         
     }
     else if(source.children.length>0 && source.children[0].getAttribute("class")==user ){
-        console.log(source.children.length);
+    
         for(var i=0;i<cur_tr_po_mov.length;i++){
             document.getElementById(cur_tr_po_mov[i]).style.background=cur_tr_po_mov_bg[i];
         }
         var idd=source.getAttribute("id");
         cur_pos=idd;
-        currenttroop=source.innerHTML;
+        currenttroop=source;
         for(var i=0;i<possmov[idd].length;i++){
             cur_tr_po_mov_bg.push(document.getElementById(possmov[idd][i]).style.backgroundColor);
             document.getElementById(possmov[idd][i]).style.background="rgb(140, 246, 255)";
@@ -561,6 +677,7 @@ function markPossible(source){
         }
     }
 }
+
 function ifCheck(){
     for(var i=0;i<8;i++){
         for(var j=0;j<8;j++){
@@ -577,6 +694,94 @@ function ifCheck(){
             return true;
         }
     }
+
+   
     return false;
 }
-
+function swapuser(){
+    if(user==human){
+        user=ai;
+    } else{
+        user=human;
+    }
+}
+function swap_cal(){
+    //swapuser();
+    return prev;
+}
+/*function checkBlocks(user){
+    var right=true,left=true;
+        var arr=[];
+        var posmov=swap_cal();          //if we use getfilter here program will colapse
+        //right side of user
+        for(var j=1;j<4;j++){
+            if(board[row][j].length>0){
+                right=false;
+                break;
+            }
+            if(j==1){
+                continue;
+            }
+            
+            for(arr in posmov){
+                if(posmov[arr].includes(row+""+j)){
+                    right=false;
+                    break;
+                }
+            }
+            
+        }
+        if(right){
+            arr[0]=row+""+2;
+        }
+        //left
+        for(var j=5;j<7;j++){
+            if(board[row][j].length>0){
+                left=false;
+                break;
+            }
+            for(arr in posmov){
+                if(posmov[arr].includes(row+""+j)){
+                    left=false;
+                    break;
+                }
+            }
+            
+        }
+        if(left){
+            arr[1]=row+""+j;
+        }
+        swapuser();
+        return arr;
+        
+}
+/*function blocks(){
+    var possmov=autocalculate();
+    for(direction in castling[user]){
+        for(emppty_blocks in castling[user][direction]){
+            console.log("length of :"+document.getElementById(castling[user][direction][emppty_blocks]).children.length>0);
+            if(document.getElementById(castling[user][direction][emppty_blocks]).children.length>0){
+                break;
+            }
+            if(castling[user][direction][emppty_blocks]=="71" || castling[user][direction][emppty_blocks]=="01"){
+                continue;
+            }
+                
+            else{
+                for(arr in possmov){
+                    if(possmov[arr].includes(castling[user][direction][emppty_blocks])){
+                        break;
+                    }
+                }
+            }
+        }
+        if(emppty_blocks>=castling[user][direction].length){
+            capable_castling[user][direction]=true;
+        }
+    }
+    console.log(capable_castling);
+}
+console.log(document.getElementById(castling[user]["left"][0]).children.length);
+console.log(castling[user]["left"][0]);
+console.log(user);
+*/
