f673e87c712f0ef0406de5c3a64b95ab6583eb15
dakshitgm <41019458+dakshitgm@users.noreply.github.com>
2020-04-12 23:06:37 +0530

    new turn
    new turn

---
+++ intialize.js
@@ -0,0 +1,52 @@
+var sqares = [];
+var cboard = document.getElementById('chessboard');
+for (var i = 0; i < 8; i++) {
+    cboard.innerHTML += "<tr id='row" + i + "'></tr>";
+    for (var j = 0; j < 8; j++) {
+        var sqare = i * 10 + j;
+        sqares.push(sqare);
+        document.getElementById('row' + i).innerHTML += '<td class="cell" id="' + sqare + '" onclick="markPossible(this)" ondrop="markPossible(this)" ondragover="prevdef(event)"></td>';
+        document.getElementById(sqare).style.backgroundColor = ((i % 2 == 0 && j % 2 == 1) || (i % 2 == 1 && j % 2 == 0)) ? "#7f5733" : "#fff1e5";
+    }
+}
+var capable_castling = {
+    white: {
+        right: true,
+        left: true
+    },
+    black: {
+        right: true,
+        left: true
+    }
+};
+//setting position of troop
+var ind = 0, jnd = 0;
+var pos = {
+    black: {
+        elephant: ['0', '7'],
+        horse: ['1', '6'],
+        camel: ['2', '5'],
+        queen: ['3'],
+        king: ['4'],
+        slder: ['10', '11', '12', '13', '14', '15', '16', '17']
+    },
+    white: {
+        elephant: ['70', '77'],
+        horse: ['71', '76'],
+        camel: ['72', '75'],
+        queen: ['73'],
+        king: ['74'],
+        slder: ['60', '61', '62', '63', '64', '65', '66', '67']
+    }
+
+};
+//place troops in it's place
+for (color in pos) {
+    for (elem in pos[color]) {
+        for (value in pos[color][elem]) {
+            document.getElementById(pos[color][elem][value]).innerHTML = "<img class='" + color + "' name='" + elem + "' src='icons/" + elem + "_" + color + ".png' draggable='true' ondragstart='markPossible(this.parentElement)'>";
+        }
+    }
+}
+var human = "white", ai = "black";
+var user = human, checkbit = false;
--- minimax.js
+++ minimax.js
@@ -1,44 +1,41 @@
-function getValue(){
-    var value=0;
-    for (var i=0;i<8;i++){
-        for(var j=0;j<8;j++){
-            if(board[i][j].length>0){
-                value += troop_vals[board[i][j][0].getAttribute("class")][board[i][j][0].getAttribute("name")];
+function getValue() {
+    var value = 0;
+    for (var i = 0; i < 8; i++) {
+        for (var j = 0; j < 8; j++) {
+            if (board[i][j].length > 0) {
+                value += troop_vals[board[i][j][0].className][board[i][j][0].name];
             }
         }
     }
     return value;
 }
-var troop_vals={
-    "black":{
-        "king":90, "queen":9, "elephant":5, "camel":3, "horse":3, "slder":1
+var troop_vals = {
+    "black": {
+        "king": 90, "queen": 9, "elephant": 5, "camel": 3, "horse": 3, "slder": 1
     },
-    "white":{
-        "king":-90, "queen":-9, "elephant":-5, "camel":-3, "horse":-3, "slder":-1
+    "white": {
+        "king": -90, "queen": -9, "elephant": -5, "camel": -3, "horse": -3, "slder": -1
     }
 };
 
-
-
 var empty = document.getElementById("empty").children;
-
-/*function nextMove() {
+function donext() {
     let bestscore = -Infinity;
     var mov = { sr: "", de: "" };
     for (sour in possmov) {
-        let sr_i = sour[0];
-        let sr_j = sour[1];
+        let sr_j = sour % 10;
+        let sr_i = (sour - sr_j) / 10;
         if (possmov[sour].length > 0) {
             for (dests in possmov[sour]) {
-                let de_i = possmov[sour][dests][0];
-                let de_j = possmov[sour][dests][1];
+                let de_j = possmov[sour][dests] % 10;
+                let de_i = (possmov[sour][dests] - de_j) / 10;
                 //let old_capcas = capable_castling;
-                if (board[sr_i][sr_j][0].getAttribute("name") == "king") {
+                if (board[sr_i][sr_j][0].name == "king") {
                     capable_castling[user]["right"] = false;
                     capable_castling[user]["left"] = false;
                 }
-                if (board[sr_i][sr_j][0].getAttribute("name") == "elephant") {
-                    if (sr_i==capable_castling[user]["i"] && sr_j == 0) {
+                if (board[sr_i][sr_j][0].name == "elephant") {
+                    if (sr_i == capable_castling[user]["i"] && sr_j == 0) {
                         capable_castling[user]["left"] = false;
                     } else if (sr_i == capable_castling[user]["i"] && sr_j == 7) {
                         capable_castling[user]["rigth"] = false;
@@ -47,6 +44,7 @@ var empty = document.getElementById("empty").children;
                 var temp = board[de_i][de_j];
                 board[de_i][de_j] = board[sr_i][sr_j];
                 board[sr_i][sr_j] = empty;
+                user = ai;
                 let score = minmax(false, 0);
                 if (score > bestscore) {
                     bestscore = score;
@@ -62,33 +60,33 @@ var empty = document.getElementById("empty").children;
     document.getElementById(mov["de"]).innerHTML = document.getElementById(mov["sr"]).innerHTML;
     document.getElementById(mov["sr"]).innerHTML = "";
     update();
-    swapuser();
+    user = human;
+    possmov = getfilter(autocalculate());
 }
 
-function minmax(ismax, depth){
-    swapuser();
+function minmax(ismax, depth) {
     if (depth >= 3) {
+        console.log(getValue());
         return getValue();
     }
-    //console.log(depth);
     if (ismax) {
         let posmv = getfilter(autocalculate());
         let bestscore = -Infinity;
         let sur;
         for (sur in posmv) {
-            let sir_i = sur[0];
-            let sir_j = sur[1];
+            let sir_j = sur % 10;
+            let sir_i = (sur - sir_j) / 10;
             if (posmv[sur].length > 0) {
                 let dests;
                 for (dests in posmv[sur]) {
-                    let de_i = posmv[sur][dests][0];
-                    let de_j = posmv[sur][dests][1];
+                    let de_j = posmv[sur][dests] % 10;
+                    let de_i = (posmv[sur][dests] - de_j) / 10;
                     //let old_capcas = capable_castling;
-                    if (board[sir_i][sir_j][0].getAttribute("name") == "king") {
+                    if (board[sir_i][sir_j][0].name == "king") {
                         capable_castling[user]["right"] = false;
                         capable_castling[user]["left"] = false;
                     }
-                    if (board[sir_i][sir_j][0].getAttribute("name") == "elephant") {
+                    if (board[sir_i][sir_j][0].name == "elephant") {
                         if (sir_i == capable_castling[user]["i"] && sir_j == 0) {
                             capable_castling[user]["left"] = false;
                         } else if (sir_i == capable_castling[user]["i"] && sir_j == 7) {
@@ -98,6 +96,7 @@ function minmax(ismax, depth){
                     var temp = board[de_i][de_j];
                     board[de_i][de_j] = board[sir_i][sir_j];
                     board[sir_i][sir_j] = empty;
+                    user = ai;
                     let score = minmax(false, depth + 1);
                     bestscore = Math.max(bestscore, score);
                     board[sir_i][sir_j] = board[de_i][de_j];
@@ -112,19 +111,19 @@ function minmax(ismax, depth){
         let bestscore = Infinity;
         let sur;
         for (sur in posmv) {
-            let sir_i = sur[0];
-            let sir_j = sur[1];
+            let sir_j = sur % 10;
+            let sir_i = (sur - sir_j) / 10;
             if (posmv[sur].length > 0) {
                 let dests;
                 for (dests in posmv[sur]) {
-                    let de_i = posmv[sur][dests][0];
-                    let de_j = posmv[sur][dests][1];
+                    let de_j = posmv[sur][dests] % 10;
+                    let de_i = (posmv[sur][dests] - de_j) / 10;
                     //let old_capcas = capable_castling;
-                    if (board[sir_i][sir_j][0].getAttribute("name") == "king") {
+                    if (board[sir_i][sir_j][0].name == "king") {
                         capable_castling[user]["right"] = false;
                         capable_castling[user]["left"] = false;
                     }
-                    if (board[sir_i][sir_j][0].getAttribute("name") == "elephant") {
+                    if (board[sir_i][sir_j][0].name == "elephant") {
                         if (sir_i == capable_castling[user]["i"] && sir_j == 0) {
                             capable_castling[user]["left"] = false;
                         } else if (sir_i == capable_castling[user]["i"] && sir_j == 7) {
@@ -134,111 +133,12 @@ function minmax(ismax, depth){
                     var temp = board[de_i][de_j];
                     board[de_i][de_j] = board[sir_i][sir_j];
                     board[sir_i][sir_j] = empty;
-                    let score = minmax(true, depth+1);
+                    user = human;
+                    let score = minmax(true, depth + 1);
                     bestscore = Math.min(bestscore, score);
                     board[sir_i][sir_j] = board[de_i][de_j];
                     board[de_i][de_j] = temp;
                     //capable_castling = old_capcas;
-                }
-            }
-        }
-        return bestscore;
-    }
-}*/
-function nextMove(){
-    var bestscore=-Infinity;
-    var move={"source":"","dest":""};
-    for(var i=0;i<8;i++){
-        for(var j=0;j<8;j++){
-            
-            if(board[i][j].length>0 && board[i][j][0].getAttribute("class")==user){
-
-                for(moves in possmov[i+""+j]){
-                    var oldboard=board;
-                    var moves_i=possmov[i+""+j][moves][0];
-                    var moves_j=possmov[i+""+j][moves][1];
-                    board[moves_i][moves_j]=board[i][j];
-                    board[i][j]=empty;
-                    var score=minimax(false,5);
-                    if(score>bestscore){
-                        bestscore=score;
-                        console.log(bestscore);
-                        move["source"]=i+""+j;
-                        move["dest"]=possmov[i+""+j][moves];
-                        console.log("here")
-                    }
-                    board=oldboard;
-                }
-            }
-        }
-    }
-    console.log(move);
-    document.getElementById(move["dest"]).innerHTML = document.getElementById(move["source"]).innerHTML;
-    document.getElementById(move["source"]).innerHTML=null ;
-    
-    update();
-    swapuser();
-    possmov=getfilter(autocalculate());
-}
-function minimax(isMax,depth){
-    if(depth ==0){
-        console.log(depth);
-        console.log(getValue());
-        return getValue();
-    }
-    
-    console.log(possmove);
-    if(isMax){
-        swapuser();
-        var possmove=getfilter(autocalculate());
-        var bestscore=-Infinity;
-        var moves;
-        for(var i=0;i<8;i++){
-            for(var j=0;j<8;j++){
-                if(board[i][j].length>0 && board[i][j][0].getAttribute("class")==user){
-                    console.log(user);
-                    for(moves in possmove[i+""+j]){
-                        var oldboard=board;
-                        var moves_i=possmove[i+""+j][moves][0];
-                        var moves_j=possmove[i+""+j][moves][1];
-                        board[moves_i][moves_j]=board[i][j];
-                        board[i][j]=empty;
-                        var score=minimax(false,depth-1);
-                        //if(score>bestscore){
-                        //    bestscore=score;
-                        //}
-                        bestscore=Math.max(bestscore,score);
-                        board=oldboard;
-                    }
-                }
-            }
-        }
-        return bestscore;
-    }
-    else{
-        swapuser();
-        var possmove=getfilter(autocalculate());
-        var bestscore=Infinity;
-        var moves;
-        for(var i=0;i<8;i++){
-            for(var j=0;j<8;j++){
-                if(board[i][j].length>0 && board[i][j][0].getAttribute("class")==user){
-                    console.log(user);
-                    console.log(possmov+""+i+""+j)
-                    for(moves in possmove[i+""+j]){
-                        var oldboard=board;
-                        console.log(possmove[i+""+j]);
-                        var moves_i=possmove[i+""+j][moves][0];
-                        var moves_j=possmove[i+""+j][moves][1];
-                        board[moves_i][moves_j]=board[i][j];
-                        board[i][j]=empty;
-                        var score=minimax(true,depth-1);
-                        //if(score<bestscore){
-                         //   bestscore=score;
-                        //}
-                        bestscore=Math.min(bestscore,score);
-                        board=oldboard;
-                    }
                 }
             }
         }
